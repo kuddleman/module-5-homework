3293bee40f542b6e627a7d44431093606ab94000
Kuddleman <edl1155@gmail.com>
2021-03-07 12:41:21 -0800

    add flight manifest class

---
+++ src/sample/FlightManifest.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FlightManifest {
+
+    private ArrayList<Traveler> travelerList;
+
+    public FlightManifest() {
+        travelerList = new ArrayList<>();
+    }
+
+    public void addTraveler(Traveler t) {
+        travelerList.add(t);
+    }
+
+    public ArrayList<Traveler> getTravelerList() {
+       return travelerList;
+    }
+}
