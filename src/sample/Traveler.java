a1c16ff44fb9a8c83c7cc0a7ac4329013a5f3ee8
Kuddleman <edl1155@gmail.com>
2021-03-07 12:22:54 -0800

    complete code for model

--- src/sample/Traveler.java
+++ src/sample/Traveler.java
@@ -1,4 +1,64 @@
 package sample;
 
 public class Traveler {
+    private String travelerName;
+    private String nationality;
+    private String passportNumber;
+    private String flightNumber;
+    private String finalDestination;
+
+    Traveler(String travelerName, String nationality,
+             String passportNumber, String flightNumber, String finalDestination) {
+
+        this.travelerName = travelerName;
+        this.nationality = nationality;
+        this.passportNumber = passportNumber;
+        this.flightNumber = flightNumber;
+        this.finalDestination = finalDestination;
+    }
+
+
+    public String getTravelerName() {
+        return travelerName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public void setFinalDestination(String finalDestination) {
+        this.finalDestination = finalDestination;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getFinalDestination() {
+        return finalDestination;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Traveler's Name: " + travelerName + "\n" +
+                "Nationality: " + nationality + "\n" +
+                "Passport Number: " + passportNumber + "\n" +
+                "Flight Number: " + flightNumber + "\n" +
+                "Final Destination: " + finalDestination;
+    }
+
+    public static void main(String[] args) {
+        Traveler donny = new Traveler("Donny", "USA", "Z4567", "PA121", "LHR");
+        System.out.println(donny);
+    }
 }
+
