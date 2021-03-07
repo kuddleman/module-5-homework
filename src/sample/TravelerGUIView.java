2421b1d95844fe5d3a354d6bb5a90fcc3142ef9c
Kuddleman <edl1155@gmail.com>
2021-03-07 12:58:37 -0800

    add variables for TravelerGUIView

---
+++ src/sample/TravelerGUIView.java
@@ -0,0 +1,26 @@
+package sample;
+
+
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+
+public class TravelerGUIView {
+
+    private TextField nameField, nationalityField,
+                      passportField, flightNumberField, finalDestinationField;
+
+    private Text introText, nameLabel, nationalityLabel,
+                 passportLabel, fligntNumberLabel, finalDestinationLabel;
+
+    private TextArea result;
+
+    private Button addButton, displayButton;
+
+    private VBox primaryBox;
+
+
+
+
+
+}
