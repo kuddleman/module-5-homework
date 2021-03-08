318e6fc212dbbe6c0135d233efa645f179750c3d
Kuddleman <edl1155@gmail.com>
2021-03-08 13:47:41 -0800

    basic coding for gui display

---
+++ src/sample/TravelerGUIController.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class TravelerGUIController extends Application {
+    private FlightManifest flightManifest;
+    private TravelerGUIView travelerInputView;
+
+    public TravelerGUIController() {
+        flightManifest = new FlightManifest();
+        travelerInputView = new TravelerGUIView();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        TravelerGUIController controller = new TravelerGUIController();
+
+        Scene scene = new Scene(controller.travelerInputView.getParent(), 800, 1200, Color.BEIGE);
+        primaryStage.setTitle("Traveler Manifest");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+}
--- src/sample/TravelerGUIView.java
+++ src/sample/TravelerGUIView.java
@@ -1,6 +1,8 @@
 package sample;
 
 
+import javafx.geometry.*;
+import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.*;
@@ -11,13 +13,37 @@ public class TravelerGUIView {
                       passportField, flightNumberField, finalDestinationField;
 
     private Text introText, nameLabel, nationalityLabel,
-                 passportLabel, fligntNumberLabel, finalDestinationLabel;
+                 passportLabel, flightNumberLabel, finalDestinationLabel;
 
     private TextArea result;
 
     private Button addButton, displayButton;
 
     private VBox primaryBox;
+
+    private final static Font font = Font.font("Helvetica", 20);
+
+    public TravelerGUIView() {
+        System.setProperty("glass.accessible.force", "false");
+        primaryBox = new VBox();
+        primaryBox.setAlignment(Pos.CENTER);
+        primaryBox.setSpacing(10);
+        primaryBox.setStyle("-fx-background-color: null");
+
+        introText = new Text("This is your Flight Manifest\n " +
+                               "Please enter the following information for each passenger:"
+                            );
+        introText.setFont(font);
+        VBox introBox = new VBox(introText);
+        introBox.setAlignment(Pos.CENTER);
+        introBox.setSpacing(5);
+        primaryBox.getChildren().add(introBox);
+
+    }
+
+    public Parent getParent() {
+        return primaryBox;
+    }
 
 
 
